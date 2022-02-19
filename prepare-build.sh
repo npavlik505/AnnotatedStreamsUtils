128d1f7cc6c05ebb105ee82aa7b40e0e8ea77b28
VanillaBrooks <brooks@karlik.org>
2022-02-18 20:47:27 -0800

    lower size on sif storage, build streams-utils in sif

--- prepare-build.sh
+++ prepare-build.sh
@@ -1,7 +1,7 @@
-sudo singularity build \
-	nv.sif \
-	"docker://nvcr.io/nvidia/nvhpc:22.1-devel-cuda_multi-ubuntu20.04"
+#sudo singularity build \
+#	nv.sif \
+#	"docker://nvcr.io/nvidia/nvhpc:22.1-devel-cuda_multi-ubuntu20.04"
 
 rm base.sif
-time SINGULARITY_TMPDIR=~/singularity sudo -E singularity build --nv base.sif build.singularity
+time SINGULARITY_TMPDIR=~/singularity sudo -E singularity build --nv base.sif base.singularity
 du -sh base.sif
